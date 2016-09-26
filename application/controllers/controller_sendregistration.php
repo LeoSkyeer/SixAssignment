ec11fbbae1e792997655185a284752cdffd147f8
Leo_Sky <vasya_90@list.ru>
2016-09-26 23:27:56 +1000

    6.1 6.2 Done - Nice Code

--- application/controllers/controller_sendregistration.php
+++ application/controllers/controller_sendregistration.php
@@ -18,8 +18,7 @@ Class controller_sendregistration extends Controller{
 
     }
 
-    public function action_send_reg()
-    {
+    private function insert_in_database(){
         if (isset($_POST['user_name']) && ($_POST['user_age']) && ($_POST['user_message'])) {
             $stmt = $this->pdo->prepare("INSERT INTO Registration_Data (name, age, text) VALUES (:name, :age, :message)");
             $stmt->execute(array(
@@ -28,41 +27,48 @@ Class controller_sendregistration extends Controller{
                 "message" => $_POST['user_message']
             ));
 
-                $path = 'photos/';
-                $ext = array_pop(explode('.', $_FILES['fileToUpload']['image'])); // расширение
-                $new_name = time() . '.' . $ext;
-                $full_path = $path . $new_name;
-                $name_in_db = substr($new_name, 0, -1);
+            $path = 'photos/';
+            $ext = array_pop(explode('.', $_FILES['fileToUpload']['image'])); // расширение
+            $new_name = time() . '.' . $ext;
+            $full_path = $path . $new_name;
+            $name_in_db = substr($new_name, 0, -1);
 //                echo $name_in_db;
-                if ($_FILES['fileToUpload']['error'] == 0) {
-                    if (substr($_FILES["fileToUpload"]["name"], -3) == "jpg" || substr($_FILES["file"]["name"], -3) == "png") {
-                        if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $full_path)) {
-                            echo "Регистрация прошла успешно";
-                        }
+            if ($_FILES['fileToUpload']['error'] == 0) {
+                if (substr($_FILES["fileToUpload"]["name"], -3) == "jpg" || substr($_FILES["file"]["name"], -3) == "png") {
+                    if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $full_path)) {
+                        echo "Регистрация прошла успешно";
                     }
                 }
+            }
 
             $stmt2 = $this->pdo->prepare("INSERT INTO Image_Data (user_id, image) VALUES ((SELECT id FROM Registration_Data WHERE NAME = :user_name), '".$name_in_db."')");
             $stmt2->bindParam(':user_name', $_POST['user_name']);
             $stmt2->execute();
-            
+
             $controller_name = 'application/controllers/controller_sendregistration.php';
             $str = strtolower($controller_name);
             new Controller_sendregistration($this->action_index());
 
         } else {
-            echo "Error";
+            echo "Error insert in Database";
         }
+    }
 
-        if (isset($_POST['g-recaptcha-response'])&& $_POST['g-recaptcha-response']) {
-        $secret = "6LfxTQcUAAAAAGgquzeyssmhaTsWEOLhGSG_GmK1";
-        $ip = $_SERVER['REMOTE_ADDR'];
-        $captcha = $_POST['g-recaptcha-response'];
-        $rsp = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secret&response=$captcha&remoteip=$ip");
-//    var_dump($rsp);
-        $arr = json_decode($rsp, true);
+    private function check_google_captcha()
+    {
+        if (isset($_POST['g-recaptcha-response']) && $_POST['g-recaptcha-response']) {
+            $secret = "6LfxTQcUAAAAAGgquzeyssmhaTsWEOLhGSG_GmK1";
+            $ip = $_SERVER['REMOTE_ADDR'];
+            $captcha = $_POST['g-recaptcha-response'];
+            $rsp = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secret&response=$captcha&remoteip=$ip");
+            $arr = json_decode($rsp, true);
+            return $arr['success'];
+        } else {
+            return false;
+        }
+    }
 
-        if ($arr['success']) {
+    private function send_email(){
             require 'vendor/autoload.php';
             $mail = new PHPMailer;
 
@@ -81,7 +87,11 @@ Class controller_sendregistration extends Controller{
             $mail->isHTML(true);                                  // Set email format to HTML
 
             $mail->Subject = 'Поздравляю ' . $_POST['user_name'] . ' с успешной регистрацией!';
-            $mail->Body = 'Твое имя: ' . ($_POST['user_name']) . '<br>' . 'Твой возраст: ' . ($_POST['user_age']) . '<br>' . 'Твоё сообщение: ' . ($_POST['user_message']) . '<br>';
+
+            $mail->Body = 'Твое имя: ' . ($_POST['user_name'])
+                . '<br>' . 'Твой возраст: ' . ($_POST['user_age']) . '<br>'
+                . 'Твоё сообщение: ' . ($_POST['user_message']) . '<br>';
+
             $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
             if (!$mail->send()) {
@@ -91,13 +101,18 @@ Class controller_sendregistration extends Controller{
             } else {
                 echo 'Message has been sent';
             }
-        }
-    } else{
-echo 'Mailer Error: ' . $this->mail->ErrorInfo;
-echo "asdasd";
 }
 
+    public function action_send_reg(){
 
-$this->view->generate('sucsess_view.php', 'template_view.php');
+        $is_captcha_succeed = $this->check_google_captcha();
+
+        if ($is_captcha_succeed) {
+            $this->insert_in_database();
+            $this->send_email();
+            $this->view->generate('sucsess_view.php', 'template_view.php');
+        }else{
+            $this->view->generate('error_view.php', 'template_view.php');
+        }
     }
 }
